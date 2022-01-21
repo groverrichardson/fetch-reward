87d51e60b47d7df259a69822d5aff4e5fe3e88bf
Grover Richardson <groverrichardson1987@gmail.com>
2022-01-21 14:29:37 -0800

    Added HTML structure for button.

--- src/components/form.js
+++ src/components/form.js
@@ -1,7 +1,73 @@
-import React from 'react';
+import {
+    Button,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+} from '@mui/material';
+import React, { useEffect } from 'react';
 
 function Form() {
-    return <main className="Form">Hi there</main>;
+    return (
+        <main className="Form">
+            <form action="/signup" method="post" id="signup">
+                <TextField
+                    required
+                    id="outlined-name"
+                    label="Name"
+                    variant="outlined"
+                />
+                <TextField
+                    required
+                    id="outlined-email"
+                    label="Email"
+                    variant="outlined"
+                />
+                <TextField
+                    required
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                />
+                <TextField
+                    required
+                    id="outlined-password-input"
+                    label="Verify Password"
+                    type="password"
+                    variant="outlined"
+                />
+                <FormControl fullWidth>
+                    <InputLabel id="occupation-select-label">
+                        Occupation
+                    </InputLabel>
+                    <Select
+                        labelId="occupation-select-label"
+                        id="occupation-select"
+                        label="occupation">
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl fullWidth>
+                    <InputLabel id="state-select-label">State</InputLabel>
+                    <Select
+                        labelId="state-select-label"
+                        id="state-select"
+                        label="state">
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button variant="contained" type="submit">
+                    Submit
+                </Button>
+            </form>
+        </main>
+    );
 }
 
 export default Form;
