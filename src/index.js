57638f5d1ccd8c848cf4bc45aad02ff93d9b2405
Grover Richardson <groverrichardson1987@gmail.com>
2022-01-21 15:12:46 -0800

    Changed tailwind extension name.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css';
+import './styles/tailwind.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
