432c7c1cca95068950c15dc810745ff99573d390
Grover Richardson <groverrichardson1987@gmail.com>
2022-01-21 15:12:07 -0800

    Cleaned up form styling and structure.

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,6 @@
 import {
     Button,
+    Container,
     FormControl,
     InputLabel,
     MenuItem,
@@ -10,63 +11,73 @@ import React, { useEffect } from 'react';
 
 function Form() {
     return (
-        <main className="Form">
-            <form action="/signup" method="post" id="signup">
-                <TextField
-                    required
-                    id="outlined-name"
-                    label="Name"
-                    variant="outlined"
-                />
-                <TextField
-                    required
-                    id="outlined-email"
-                    label="Email"
-                    variant="outlined"
-                />
-                <TextField
-                    required
-                    id="outlined-password-input"
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                />
-                <TextField
-                    required
-                    id="outlined-password-input"
-                    label="Verify Password"
-                    type="password"
-                    variant="outlined"
-                />
-                <FormControl fullWidth>
-                    <InputLabel id="occupation-select-label">
-                        Occupation
-                    </InputLabel>
-                    <Select
-                        labelId="occupation-select-label"
-                        id="occupation-select"
-                        label="occupation">
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl fullWidth>
-                    <InputLabel id="state-select-label">State</InputLabel>
-                    <Select
-                        labelId="state-select-label"
-                        id="state-select"
-                        label="state">
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-                <Button variant="contained" type="submit">
-                    Submit
-                </Button>
-            </form>
-        </main>
+        <div className="Form h-screen flex flex-col justify-center content-center">
+            <Container maxWidth="sm">
+                <form
+                    action="/signup"
+                    method="post"
+                    id="signup"
+                    className="grid">
+                    <TextField
+                        required
+                        id="outlined-name"
+                        label="Name"
+                        variant="outlined"
+                    />
+                    <TextField
+                        required
+                        id="outlined-email"
+                        label="Email"
+                        variant="outlined"
+                    />
+                    <TextField
+                        required
+                        id="outlined-password-input"
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                    />
+                    <TextField
+                        required
+                        id="outlined-password-input"
+                        label="Verify Password"
+                        type="password"
+                        variant="outlined"
+                    />
+                    <FormControl fullWidth>
+                        <InputLabel id="occupation-select-label">
+                            Occupation
+                        </InputLabel>
+                        <Select
+                            labelId="occupation-select-label"
+                            id="occupation-select"
+                            label="occupation">
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl fullWidth>
+                        <InputLabel id="state-select-label">State</InputLabel>
+                        <Select
+                            labelId="state-select-label"
+                            id="state-select"
+                            label="state">
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button
+                        id="submit-button"
+                        className="w-full"
+                        variant="contained"
+                        type="submit">
+                        Submit
+                    </Button>
+                </form>
+            </Container>
+        </div>
     );
 }
 
